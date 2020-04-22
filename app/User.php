20f0753c0edc6a4d1ba86b38e7551d8690795fcb
Alysson Marques <alyssonmarques@gmail.com>
2020-04-22 17:45:45 -0300

    Faltou um import :)

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use App\DistribuicaoDemanda;
+use App\Mail\RecuperarSenha;
 
 class User extends Authenticatable implements JWTSubject
 {
