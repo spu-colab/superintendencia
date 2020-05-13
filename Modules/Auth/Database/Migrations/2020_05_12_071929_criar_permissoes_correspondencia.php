d7cfa62dd213f5c01aa325623e5db744726cf928
Nildo Azevedo <nildo.azevedo@mp.intra>
2020-05-13 18:42:08 -0300

    corr classe migration criar permissoes corresp...

---
+++ Modules/Auth/Database/Migrations/2020_05_12_071929_criar_permissoes_correspondencia.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarPermissoesCorrespondencia extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('permissao')->insert(
+            [
+                [
+                    'permissao' => 'CORRESPONDENCIA_CORRESPONDENCIA_POSTAR',
+                    'descricao' => 'Permite ao usuário da área de apoio gerir postagens'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $permissao = DB::table('permissao')->where('permissao', 'CORRESPONDENCIA_CORRESPONDENCIA_POSTAR')->first();
+        if(@$permissao->id) {
+            DB::table('usuariopermissao')->where('idPermissao', '=', $permissao->id)->delete();
+        }
+        DB::table('permissao')->where('permissao', '=', 'CORRESPONDENCIA_CORRESPONDENCIA_POSTAR')->delete();
+    }
+}
