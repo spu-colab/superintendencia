b6a094cc4ac2b793ab3201d9d4ca1e4b11b20d18
Alysson Marques <alysson.marques@planejamento.gov.br>
2020-07-15 23:40:13 -0300

    Correção nas rotas de Patrimonio

--- Modules/Patrimonio/Routes/api.php
+++ Modules/Patrimonio/Routes/api.php
@@ -15,13 +15,13 @@ use Illuminate\Http\Request;
 Route::middleware('jwt.auth')
     ->namespace("\Modules\Patrimonio\Http\Controllers")
     ->group(function () {
-    Route::apiResource('patrimonio', 'PatrimonioController');
-    Route::get('patrimonio/tabelas', 'PatrimonioController@tabelas');
-    Route::get('patrimonio/divisao', 'PatrimonioController@divisao');
-    Route::post('patrimonio/gerarRelatorio', 'PatrimonioController@gerarRelatorio');
-    Route::put('patrimonio/editar/{id}', 'PatrimonioController@editar');
-    Route::put('patrimonio/editarLocal/{id}', 'PatrimonioController@editarLocal');
-    Route::get('patrimonio/gerarTermo/{id}', 'PatrimonioController@gerarTermo');
-    Route::post('patrimonio/criar', 'PatrimonioController@criar');
-    Route::post('patrimonio/criarLocal', 'PatrimonioController@criarLocal');
-});
+        Route::get('patrimonio/divisao', 'PatrimonioController@divisao');
+        Route::get('patrimonio/tabelas', 'PatrimonioController@tabelas');
+        Route::apiResource('patrimonio', 'PatrimonioController');
+        Route::post('patrimonio/gerarRelatorio', 'PatrimonioController@gerarRelatorio');
+        Route::put('patrimonio/editar/{id}', 'PatrimonioController@editar');
+        Route::put('patrimonio/editarLocal/{id}', 'PatrimonioController@editarLocal');
+        Route::get('patrimonio/gerarTermo/{id}', 'PatrimonioController@gerarTermo');
+        Route::post('patrimonio/criar', 'PatrimonioController@criar');
+        Route::post('patrimonio/criarLocal', 'PatrimonioController@criarLocal');
+    });
