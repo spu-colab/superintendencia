2d45438d6016f5b98706f25e25a0f8a76015d65d
Alysson Marques <alysson.marques@planejamento.gov.br>
2020-08-10 18:54:04 -0300

    #17

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Builder::defaultStringLength(191);
+        Schema::defaultStringLength(191);
     }
 }
